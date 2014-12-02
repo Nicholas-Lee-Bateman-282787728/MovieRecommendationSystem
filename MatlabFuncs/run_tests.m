53eb1f4d95d55a485de422d1e2e08fce5a5e2325
Pulkit Gupta <pulkit2016@gmail.com>
2014-12-01 20:43:26 -0600

    adding batch script and results

--- MatlabFuncs/run_tests.m
+++ MatlabFuncs/run_tests.m
@@ -1,1 +1,6 @@
-length(UserData(:,1))
+numberOfCases = length(UserData(:,1));
+%Configuration parameters
+k = 5;  %Number of best matching map units to examine
+n = 20; %Number of movie recommendations to return
+for i=1:numberOfCases
+    RecommendedMovies = RecommendMoviesForUser( i, UserData, sM, sD, k, n, 'addandreduceto1')
